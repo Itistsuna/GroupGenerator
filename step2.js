18a935d1ca2e30ab0fcd95c8245bd230fa6fffa0
Itistsuna <46030435+Itistsuna@users.noreply.github.com>
2020-07-24 16:46:02 +0200

    Add files via upload

--- step2.js
+++ step2.js
@@ -1,122 +1,193 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const client = new MongoClient('mongodb://127.0.0.1:27017')
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient("mongodb://127.0.0.1:27017", {
+    useUnifiedTopology: true,
+});
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 // REQUETE POST  STUDENTS ---------------------------------------------------------------------------------->
 
-app.post('/students', (req, res) => {
+app.post("/students", (req, res) => {
     async function post() {
         try {
-            var data = req.body
-            await client.connect()
-            data.name.forEach(element => {
-                envoie = { name: element }
-                client.db('GroupGenerator').collection('Students').insertOne(envoie)
+            var data = req.body;
+            await client.connect();
+            data.name.forEach((element) => {
+                envoie = {
+                    name: element,
+                };
+                client.db("GroupGenerator").collection("Students").insertOne(envoie);
             });
         } catch (e) {
-            console.log(e)
-        } 
+            console.log(e);
+        }
     }
-    post()
-    res.send('Recues')
-})
+    post();
+    res.send("Recues");
+});
 
 // REQUETE GET STUDENTS ---------------------------------------------------------------------------------->
 
-app.get('/students', (req, res) => {
+app.get("/students", (req, res) => {
     async function get() {
         try {
-            await client.connect()
-            let etudiants = await client.db('GroupGenerator').collection('Students').find().toArray()
-            res.json(etudiants)
+            await client.connect();
+            let etudiants = await client
+                .db("GroupGenerator")
+                .collection("Students")
+                .find()
+                .toArray();
+            res.json(etudiants);
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
     }
 
-    get()
-
-
-
-})
+    get();
+});
 // REQUETE DELETE STUDENTS ---------------------------------------------------------------------------->
-app.delete('/students', (req, res) => {
+app.delete("/students", (req, res) => {
     async function effaccer() {
-        try{
-            await client.connect()
-            let student = await client.db('GroupGenerator').collection('Students').find().toArray()
-            student.forEach(element => {
-                client.db('GroupGenerator').collection('Students').deleteOne(element)
+        try {
+            await client.connect();
+            let student = await client
+                .db("GroupGenerator")
+                .collection("Students")
+                .find()
+                .toArray();
+            student.forEach((element) => {
+                client.db("GroupGenerator").collection("Students").deleteOne(element);
             });
-            res.send('cest bien effacer')
-        } catch(e){
-            console.log(e)
+            res.send("cest bien effacer");
+        } catch (e) {
+            console.log(e);
         }
     }
-    effaccer()
-})
+    effaccer();
+});
 
 // REQUETE POST GROUP ---------------------------------------------------------------------------------->
 
-app.post('/groups', (req, res) => {
+app.post("/groups", (req, res) => {
     async function post() {
         try {
-            var data = req.body
-            await client.connect()
-            data.name.forEach(element => {
-                envoie = { group: element }
-                client.db('GroupGenerator').collection('Groups').insertOne(envoie)
-            });
+            var data = req.body;
+            await client.connect();
+            client.db("GroupGenerator").collection("Groups").insertOne(data);
         } catch (e) {
-            console.log(e)
-        } 
+            console.log(e);
+        }
     }
-    post()
-    res.send('Recues')
-})
+    post();
+    res.send("Recues");
+});
 
 //REQUETE GET GROUP ----------------------------------------------------------------------------------------->
 
-app.get('/groups', (req, res) => {
+app.get("/groups", (req, res) => {
     async function get() {
         try {
-            await client.connect()
-            let group = await client.db('GroupGenerator').collection('Groups').find().toArray()
-            res.json(group)
+            await client.connect();
+            let group = await client
+                .db("GroupGenerator")
+                .collection("Groups")
+                .find()
+                .toArray();
+            res.json(group);
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
     }
 
-    get()
-})
+    get();
+});
 
-// REQUETE DELETE GROUP ----------------------------------------------------------------------------------------->
+// REQUETE GET GROUP:NAME ------------------------------------------------------------------------------------------>
 
-app.delete('/groups', (req, res) => {
+app.get("/groups/:grName", (req, res) => {
+    async function get() {
+        try {
+            await client.connect();
+            let id = req.params.grName;
+            console.log(id);
+            let groupe = await client
+                .db("GroupGenerator")
+                .collection("Groups")
+                .find({
+                    grpName: id
+                })
+                .toArray();
+            if (groupe.length != 0) {
+                res.status(200).send(groupe);
+            } else {
+                res.status(404).send("Error 404 , Groupe inexitant");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    get();
+});
+
+// REQUETE DELETE GROUP ALL----------------------------------------------------------------------------------------->
+
+app.delete("/groups", (req, res) => {
     async function effaccer() {
-        try{
-            await client.connect()
-            let group = await client.db('GroupGenerator').collection('Groups').find().toArray()
-            group.forEach(element => {
-                client.db('GroupGenerator').collection('Groups').deleteOne(element)
+        try {
+            await client.connect();
+            let group = await client
+                .db("GroupGenerator")
+                .collection("Groups")
+                .find()
+                .toArray();
+            group.forEach((element) => {
+                client.db("GroupGenerator").collection("Groups").deleteOne(element);
             });
-            res.send('cest bien effacer')
-        } catch(e){
-            console.log(e)
+            res.send("cest bien effacer");
+        } catch (e) {
+            console.log(e);
         }
     }
-    effaccer()
-})
+    effaccer();
+});
 
+// REQUETE DELETE GROUP:name----------------------------------------------------------------------------------------->
 
-
+app.delete("/groups/:name", (req, res) => {
+    async function effaccer() {
+        try {
+            await client.connect();
+            var id = req.params.name;
+            let group = await client
+                .db("GroupGenerator")
+                .collection("Groups")
+                .find({
+                    grpName: id.toUpperCase(),
+                })
+                .toArray();
+            console.log(group);
+            if (group.length != 0) {
+                group.forEach(element => {
+                client.db("GroupGenerator").collection("Groups").deleteOne(element);
+                });
+                res.send("cest bien effacer");
+            } else {
+                res.send("Error 404");
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    effaccer();
+});
 
 app.listen(3000, function () {
-    console.log('listening on 3000')
-})
+    console.log("listening on 3000");
+});
---
+++ step3.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const fs = require('fs')
+const MongoClient = require('mongodb').MongoClient
+const client = new MongoClient('mongodb://127.0.0.1:27017')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+
+// REQUETE GET FRONT STUDENTS ------------------------------------------------------------------------------------->
+
+app.get('/students', (req, res) => {
+    try {
+        const template = fs.readFileSync('./student.ejs', 'utf-8')
+        var html = ejs.render(template)
+        res.send(html)
+    } catch (e) {
+        console.log(e)
+    }
+})
+// REQUETE GET FRONT GROUP ---------------------------------------------------------------------------------->  
+
+app.get('/groups', (req, res) => {
+    try {
+        const template = fs.readFileSync('./groups.ejs', 'utf-8')
+        var html = ejs.render(template)
+        res.send(html)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+app.listen(3000, function () {
+    console.log('listening on 3000')
+})
