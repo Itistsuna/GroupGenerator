2306f74935422e0045236ede8da7f6a5c2f5d7e7
Itistsuna <46030435+Itistsuna@users.noreply.github.com>
2020-07-24 12:57:15 +0200

    Add files via upload

--- step2.js
+++ step2.js
@@ -7,29 +7,26 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-// REQUETE POST ----------------------------------------------------------------------------------
+// REQUETE POST  STUDENTS ---------------------------------------------------------------------------------->
+
 app.post('/students', (req, res) => {
     async function post() {
         try {
             var data = req.body
             await client.connect()
             data.name.forEach(element => {
-                console.log(element)
                 envoie = { name: element }
-                console.log(envoie)
                 client.db('GroupGenerator').collection('Students').insertOne(envoie)
             });
         } catch (e) {
             console.log(e)
-        } finally {
-            await client.close()
-        }
+        } 
     }
     post()
     res.send('Recues')
 })
 
-// REQUETE GET ----------------------------------------------------------------------------------
+// REQUETE GET STUDENTS ---------------------------------------------------------------------------------->
 
 app.get('/students', (req, res) => {
     async function get() {
@@ -39,8 +36,6 @@ app.get('/students', (req, res) => {
             res.json(etudiants)
         } catch (e) {
             console.log(e)
-        } finally {
-            await client.close()
         }
     }
 
@@ -49,24 +44,78 @@ app.get('/students', (req, res) => {
 
 
 })
-// REQUETE DELETE ---------------------------------------------------------------------------->
-app.delete('/students', (req,res) => {
+// REQUETE DELETE STUDENTS ---------------------------------------------------------------------------->
+app.delete('/students', (req, res) => {
     async function effaccer() {
         try{
             await client.connect()
-            let student = await client.db('GroupGenerator').collection('Students').deleteMany(student)
+            let student = await client.db('GroupGenerator').collection('Students').find().toArray()
+            student.forEach(element => {
+                client.db('GroupGenerator').collection('Students').deleteOne(element)
+            });
             res.send('cest bien effacer')
-
         } catch(e){
             console.log(e)
-
-        }
-        finally{
-            await client.close();
         }
     }
     effaccer()
 })
+
+// REQUETE POST GROUP ---------------------------------------------------------------------------------->
+
+app.post('/groups', (req, res) => {
+    async function post() {
+        try {
+            var data = req.body
+            await client.connect()
+            data.name.forEach(element => {
+                envoie = { group: element }
+                client.db('GroupGenerator').collection('Groups').insertOne(envoie)
+            });
+        } catch (e) {
+            console.log(e)
+        } 
+    }
+    post()
+    res.send('Recues')
+})
+
+//REQUETE GET GROUP ----------------------------------------------------------------------------------------->
+
+app.get('/groups', (req, res) => {
+    async function get() {
+        try {
+            await client.connect()
+            let group = await client.db('GroupGenerator').collection('Groups').find().toArray()
+            res.json(group)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    get()
+})
+
+// REQUETE DELETE GROUP ----------------------------------------------------------------------------------------->
+
+app.delete('/groups', (req, res) => {
+    async function effaccer() {
+        try{
+            await client.connect()
+            let group = await client.db('GroupGenerator').collection('Groups').find().toArray()
+            group.forEach(element => {
+                client.db('GroupGenerator').collection('Groups').deleteOne(element)
+            });
+            res.send('cest bien effacer')
+        } catch(e){
+            console.log(e)
+        }
+    }
+    effaccer()
+})
+
+
+
 
 app.listen(3000, function () {
     console.log('listening on 3000')
