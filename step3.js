2934d34c8d92fa9a4de8f0ff2feb5b0d2a2afbdb
Itistsuna <46030435+Itistsuna@users.noreply.github.com>
2020-07-28 16:01:49 +0200

    Add files via upload

--- step3.js
+++ step3.js
@@ -3,8 +3,10 @@ const app = express()
 const ejs = require('ejs')
 const fs = require('fs')
 const fetch = require('node-fetch')
-const bodyParser = require('body-parser');
-const { json } = require('body-parser')
+const {
+    json
+} = require('body-parser')
+const { group } = require('console')
 app.use(express.static(__dirname + '/static'));
 app.use(express.json())
 app.use(express.urlencoded({
@@ -39,7 +41,7 @@ app.get('/students', (req, res) => {
                     students: students
                 })
                 res.send(html)
-            } catch(e){
+            } catch (e) {
                 console.log(e)
             }
         }
@@ -54,16 +56,20 @@ app.get('/students', (req, res) => {
 app.post("/students", (req, res) => {
     async function post() {
         try {
-            fetch('http://localhost:8080/students',{
-                method: "post",
-                headers: { "Content-Type": "application/json"},
-                body: JSON.stringify({
-                    name: req.body.name.toUpperCase()
-                })
-            }).then((res) => res.json())
-            .then(json => console.log(json))
+            fetch('http://localhost:8080/students', {
+                    method: "post",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        name: req.body.name.toUpperCase()
+                    })
+                }).then((res) => res.json())
+                .then(json => console.log(json))
             const template = fs.readFileSync('./student.ejs', 'utf-8')
-            students[0].push({name: req.body.name.toUpperCase()})
+            students[0].push({
+                name: req.body.name.toUpperCase()
+            })
             var html = ejs.render(template, {
                 students: students
             })
@@ -92,14 +98,44 @@ app.get('/groups', (req, res) => {
 
 //REQUETE POST FRONT GROUP  --------------------------------------------------------------------------------->
 app.post('/groups', (req, res) => {
-    res.send(req.body)
+    let name =[]
+    var index = students[0].length;
+    for(let i = 1; i <= req.body.nbrPersonnes ;i++){
+        random = Math.floor(Math.random() * (index))
+        name1 = students[0][random]
+        name.push(name1.name)
+        students[0].splice(random, 1)
+        index = students[0].length
+    }
     class GROUPE {
         constructor() {
             this.grpName = req.body.grpName;
             this.nbrPersonnes = req.body.nbrPersonnes;
+            this.name = name
         }
     }
     let NGroupe = new GROUPE
+    groupe[0].push(NGroupe)
+    async function post(){
+        try {
+            fetch('http://localhost:8080/groups', {
+                    method: "post",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(NGroupe)
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    post()
+    const template = fs.readFileSync('./groups.ejs', 'utf-8')
+    var html = ejs.render(template, {
+        groupe: groupe
+    })
+    res.send(html)
+
 })
 
 app.listen(3000, function () {
