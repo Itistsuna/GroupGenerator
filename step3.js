e6e88b3fea803d020a96c9c24ced77efce685c1a
Itistsuna <46030435+Itistsuna@users.noreply.github.com>
2020-07-29 11:49:59 +0200

    Add files via upload

--- step3.js
+++ step3.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const fs = require('fs');
-const alert = require('alert-node');
+const alert = require('alert');
 const fetch = require('node-fetch');
-const { json } = require('body-parser');
-const { group } = require('console');
+const {
+	json
+} = require('body-parser');
+const {
+	group
+} = require('console');
 app.use(express.static(__dirname + '/static'));
 app.use(express.json());
 app.use(
@@ -26,21 +30,30 @@ async function data() {
 		const jsonGroupe = await responseGroupe.json();
 		// Supprimer les etudiant deja dans des groupes
 		for (i = 0; i < jsonStudents.length; i++) {
-            console.log(jsonStudents)
 			for (let index = 0; index < jsonGroupe.length; index++) {
-                if (jsonStudents.length == 0){
-                    console.log('Il n\'y a plus d\'étudiant')
-                }
-				else if (jsonStudents[i].name == jsonGroupe[index].name[0]) {
+				if (jsonStudents.length == 0) {
+					console.log('Il n\'y a plus d\'étudiant')
+				} else if (jsonStudents[i].name == jsonGroupe[0].name[index]) {
 					jsonStudents.splice(i, 1);
-					i = i - 1;
-				} else if (jsonStudents[i].name == jsonGroupe[index].name[1]) {
+					if (i != 0) {
+						i = i - 1;
+						break
+					} else {
+						i = -1
+						break
+					}
+				} else if (jsonStudents[i].name == jsonGroupe[1].name[index]) {
 					jsonStudents.splice(i, 1);
-					i = i - 1;
+					if (i != 0) {
+						i = i - 1;
+						break
+					} else {
+						i = -1
+						break
+					}
 				}
 			}
-        }
-        console.log(jsonStudents)
+		}
 		students.push(jsonStudents);
 		groupe.push(jsonGroupe);
 	} catch (e) {
@@ -74,21 +87,25 @@ app.get('/students', (req, res) => {
 app.post('/students', (req, res) => {
 	async function post() {
 		try {
-			fetch('http://localhost:8080/students', {
-				method: 'post',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					name: req.body.name.toUpperCase()
-				})
-			})
-				.then((res) => res.json())
-				.then((json) => console.log(json));
+			if (req.body.name === '') {
+				alert('Veuiller écrire un nom d\'étudiant')
+			} else {
+				fetch('http://localhost:8080/students', {
+						method: 'post',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({
+							name: req.body.name.toUpperCase()
+						})
+					})
+					.then((res) => res.json())
+					.then((json) => console.log(json));
+					students[0].push({
+						name: req.body.name.toUpperCase()
+					});
+			}
 			const template = fs.readFileSync('./student.ejs', 'utf-8');
-			students[0].push({
-				name: req.body.name.toUpperCase()
-			});
 			var html = ejs.render(template, {
 				students: students
 			});
@@ -118,31 +135,31 @@ app.get('/groups', (req, res) => {
 app.post('/groups', (req, res) => {
 	let name = [];
 	var index = students[0].length;
-	if (students[0].length <= 1) {
-		alert('Veuillez rajouter des étudiants');
+	if (req.body.grpName === '') {
+		alert('Nom du projet manquant');
+	} else if (req.body.nbrPersonnes === '') {
+		alert('Nombres de personnes manquantes');
 	} else {
-		for (let i = 1; i <= req.body.nbrPersonnes; i++) {
-			random = Math.floor(Math.random() * index);
-			name1 = students[0][random];
-			name.push(name1.name);
-			students[0].splice(random, 1);
-			index = students[0].length;
-		}
-		console.log(name);
-		console.log(req.body);
-		class GROUPE {
-			constructor() {
-				this.grpName = req.body.grpName;
-				this.nbrPersonnes = req.body.nbrPersonnes;
-				this.name = name;
-			}
-		}
-		let NGroupe = new GROUPE();
-		console.log(NGroupe);
-		groupe[0].push(NGroupe);
-		if (NGroupe.grpName === '') {
-			alert('Nom de groupe manquant');
+		if (students[0].length <= 1) {
+			alert('Veuillez rajouter des étudiants');
 		} else {
+			for (let i = 1; i <= req.body.nbrPersonnes; i++) {
+				random = Math.floor(Math.random() * index);
+				name1 = students[0][random];
+				name.push(name1.name);
+				students[0].splice(random, 1);
+				index = students[0].length;
+			}
+			class GROUPE {
+				constructor() {
+					this.grpName = req.body.grpName;
+					this.nbrPersonnes = req.body.nbrPersonnes;
+					this.name = name;
+				}
+			}
+			let NGroupe = new GROUPE();
+			groupe[0].push(NGroupe);
+
 			async function post() {
 				try {
 					await fetch('http://localhost:8080/groups', {
@@ -167,6 +184,6 @@ app.post('/groups', (req, res) => {
 	res.send(html);
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
 	console.log('listening on port 3000');
 });
